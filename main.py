f5b619697d4aab283184924b869ad9387dd9f593
kwa_laa <khoaddnguyen@gmail.com>
2023-08-29 13:21:49 -0500

    add password generator

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+# for char in range(nr_letters):
+#   password_list.append(random.choice(letters))
+#
+# for char in range(nr_symbols):
+#   password_list += random.choice(symbols)
+#
+# for char in range(nr_numbers):
+#   password_list += random.choice(numbers)
+
+# List Comprehension: new_list = [new_item for item in list]
+password_letters = [random.choice(letters) for _ in range(0, nr_letters)]
+password_symbols = [random.choice(symbols) for _ in range(0, nr_symbols)]
+password_numbers = [random.choice(numbers) for _ in range(0, nr_numbers)]
+
+password_list = password_letters + password_symbols + password_numbers
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
