a3a000eaabe2b5d32180271ea7351a718d891170
kwa_laa <khoaddnguyen@gmail.com>
2023-08-29 13:42:13 -0500

    password generator now available on Py window. Add pyperclip library to auto copy pw to clipboard.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
@@ -9,9 +10,6 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
 
 # for char in range(nr_letters):
 #   password_list.append(random.choice(letters))
@@ -23,18 +21,24 @@ nr_numbers = random.randint(2, 4)
 #   password_list += random.choice(numbers)
 
 # List Comprehension: new_list = [new_item for item in list]
-password_letters = [random.choice(letters) for _ in range(0, nr_letters)]
-password_symbols = [random.choice(symbols) for _ in range(0, nr_symbols)]
-password_numbers = [random.choice(numbers) for _ in range(0, nr_numbers)]
+def generate_password():
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-password_list = password_letters + password_symbols + password_numbers
-random.shuffle(password_list)
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+    # password = ""
+    # for char in password_list:
+    #   password += char
 
-print(f"Your password is: {password}")
+    password = "".join(password_list)
+    pw_entry.insert(0, password)  # insert password to the entry box
+    pyperclip.copy(password)  # auto copy pw to clipboard to get ready for pasting
+
+
+    #print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
@@ -100,7 +104,7 @@ pw_entry.grid(column=1, row=3)
 
 # Buttons
 
-generate_pw_button = Button(text="Generate Password",width=11)
+generate_pw_button = Button(text="Generate Password",width=11, command=generate_password)
 generate_pw_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=33, command=save)
